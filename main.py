f9918fa4252bd9b62885152dbd6f5c7b1c75e70c
Quirax Lee <quiraxical@gmail.com>
2022-05-29 16:36:31 +0900

    Finalize Project

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import sys
+import pickle
 
 # MODES
 DEBUG_MODE       = True
@@ -13,6 +15,7 @@ USE_PICKLE       = True
 # PARAMETERS
 TRAIN_FILENAME   = 'dataset/train.json'
 TEST_FILENAME    = 'dataset/test.json'
+QUERY_FILENAME   = 'query.json'
 ENCODING         = 'utf-8'
 VECTOR_SIZE      = 300
 NUM_OF_SENTENCES = 6
@@ -25,13 +28,12 @@ STOP_WORDS       = {
 OPTIMIZER_PARAMS = {'lr': 1} # lr = 1
 MAX_EPOCH        = 20000
 LOG_EPOCH        = 100
-RANDOM_SEED      = 85624965 # 85624965 (Alt. 91263498)
+RANDOM_SEED      = 3484336315 # 3484336315 (Alt. 85624965)
 RNN_MODEL        = nn.RNN
 RNN_SIZE         = 20 # 20
 RNN_LAYERS       = 1
 RNN_PARAMS       = {}
 FC1_SIZE         = 4 # 4
-DROPOUT_RATE     = 0.2
 
 # Model
 class Model(nn.Module):
@@ -52,22 +54,36 @@ class Model(nn.Module):
         fc1 = outputs.squeeze(0)
         fc1 = self.fc1(fc1)
         fc1 = F.relu(fc1)
-        fc2 = self.fc3(fc1)
+        fc2 = self.fc2(fc1)
         softmax = self.softmax(fc2)
         return softmax
 
-# Sequence or Tqdm
+# Output mode
 def seq(s, desc):
     if DEBUG_MODE:
-        return tqdm(s, desc=desc)
+        return tqdm(s, desc=desc, file = sys.stderr)
     else:
         return s
 
-# Dataset validator
+def pprint(s):
+    if DEBUG_MODE:
+        print(s, file=sys.stderr)
+
+# Dataset preprocessors
 def validate_dataset(dataset):
     for item in dataset:
         if len(item['sentences']) != NUM_OF_SENTENCES:
             raise 'ERROR: The number of sentences is invalid'
+
+def preprocessor(dataset, nlp):
+    validate_dataset(dataset)
+        
+    for i, item in enumerate(seq(dataset, "Making vectors for each sentences")):
+        dataset[i]['vectors'] = []
+        
+        for sentence in item['sentences']:
+            tokens = nlp(sentence)
+            dataset[i]['vectors'].append(tokens.vector)
 
 # Trainer
 def do_train(model, dataset, loss_fn, optimizer):
@@ -94,56 +110,87 @@ def do_train(model, dataset, loss_fn, optimizer):
     return train_loss / n_data, n_correct / n_data
 
 # Evaluater
-def evaluate(model, dataset, loss_fn):
+def evaluate(model, dataset, loss_fn, doPrint=False):
+    def eprint(s):
+        if doPrint: print(s)
+
     model.eval()
     test_loss, n_correct, n_data = 0, 0, 0
 
     with torch.no_grad():
-        for item in dataset:
+        for i, item in enumerate(dataset):
+            eprint(f"")
+            for j, s in enumerate(item['sentences']):
+                if j == 0: eprint(f"{i+1:>3}. {s}")
+                else: eprint(f" ({j}) {s}")
+            
             data = torch.asarray(np.array([item['vectors']]))
-
-            target = torch.zeros((1, NUM_OF_CHOICES), dtype=torch.float)
-            target[0][item['answer'] - 1] = 1.0
-
             output = model(data)
-            loss = loss_fn(output, target)
             predict = torch.argmax(output, dim=1)
 
-            test_loss += loss.item()
-            if predict + 1 == item['answer']: n_correct += 1
-            n_data += 1
+            eprint(f"Predict: {predict[0] + 1}")
+
+            if item['answer']:
+                target = torch.zeros((1, NUM_OF_CHOICES), dtype=torch.float)
+                target[0][item['answer'] - 1] = 1.0
+                loss = loss_fn(output, target)
+
+                eprint(f"Actual Answer: {item['answer']}")
+
+                test_loss += loss.item()
+                if predict + 1 == item['answer']: n_correct += 1
+                n_data += 1
+            
+            eprint(f"\n{'-'*10}\n")
     
     return test_loss / n_data, n_correct / n_data
 
 # Pickler
+REQUIRED_SAVE_MODEL = False
+
+def save_model(model):
+    with open('model.pickle', 'wb') as f:
+        pickle.dump(model, f)
+
+def load_model():
+    global REQUIRED_SAVE_MODEL
+    try:
+        with open('model.pickle', 'rb') as f:
+            model = pickle.load(f)
+        
+        return model
+    except:
+        REQUIRED_SAVE_MODEL = True
+        return None
+
 if USE_PICKLE:
-    REQUIRED_SAVE = False
-
-    import pickle
-
-    def save_pickle(train, test):
+    REQUIRED_SAVE_DATASETS = False
+    
+    def save_datasets(train, test):
         with open('datasets.pickle', 'wb') as f:
             pickle.dump((train, test), f)
     
-    def load_pickle():
-        global REQUIRED_SAVE
+    def load_datasets():
+        global REQUIRED_SAVE_DATASETS
         try:
             with open('datasets.pickle', 'rb') as f:
                 train, test = pickle.load(f)
             
             return train, test
         except:
-            REQUIRED_SAVE = True
+            REQUIRED_SAVE_DATASETS = True
             return None, None
 
-# Load datasets
+# Load pickles
 if USE_PICKLE:
-    train, test = load_pickle()
-    if REQUIRED_SAVE:
+    train, test = load_datasets()
+    if REQUIRED_SAVE_DATASETS:
         train_fp = open(TRAIN_FILENAME, 'r', encoding=ENCODING)
         test_fp  = open(TEST_FILENAME,  'r', encoding=ENCODING)
         train    = json.load(train_fp)
         test     = json.load(test_fp)
+    
+    model = load_model()
 
 # Load predefined english model
 nlp = spacy.load('en_core_web_lg')
@@ -152,39 +199,36 @@ nlp.Defaults.stop_words |= STOP_WORDS
 
 torch.manual_seed(RANDOM_SEED)
 
+query_fp = open(QUERY_FILENAME,  'r', encoding=ENCODING)
+query    = json.load(query_fp)
+
 if __name__ == '__main__':
-    if not USE_PICKLE or REQUIRED_SAVE:
+    if not USE_PICKLE or REQUIRED_SAVE_DATASETS:
         # Validate dataset
         for dataset in (train, test):
-            validate_dataset(dataset)
-        
-            for i, item in enumerate(seq(dataset, "Making vectors for each sentences")):
-                dataset[i]['vectors'] = []
-                
-                for sentence in item['sentences']:
-                    tokens = nlp(sentence)
-                    dataset[i]['vectors'].append(tokens.vector)
+            preprocessor(dataset, nlp)
 
-        if USE_PICKLE: save_pickle(train, test)
+        if USE_PICKLE: save_datasets(train, test)
 
-    model = Model()
-    target_model = model
     loss = F.cross_entropy
-    optimizer = torch.optim.Adadelta(model.parameters(), **OPTIMIZER_PARAMS)
 
-    p_train_loss, p_train_acc, p_valid_loss, p_valid_acc = 0, 0, 0, 0
-    t_valid_loss, t_valid_acc = 0, 0
+    if not USE_PICKLE or REQUIRED_SAVE_MODEL:
+        model = Model()
+        optimizer = torch.optim.Adadelta(model.parameters(), **OPTIMIZER_PARAMS)
 
-    for epoch in range(1, MAX_EPOCH + 1):
-        train_loss, train_acc = do_train(model, train, loss, optimizer)
-        valid_loss, valid_acc = evaluate(model, test, loss)
+        t_valid_loss, t_valid_acc, t_epoch = 0, 0, 0
 
-        if (valid_loss < t_valid_loss and valid_acc >= t_valid_acc) or epoch % LOG_EPOCH == 0:
-            print(f"{epoch: >6} => TrLoss={train_loss:.6f}({train_loss - p_train_loss:.6f}), TrAcc={train_acc:.3f}({train_acc - p_train_acc:.3f}), " \
-                f"VaLoss={valid_loss:.6f}({valid_loss - p_valid_loss:.6f}), VaAcc={valid_acc:.3f}({valid_acc - p_valid_acc:.3f})")
-            if valid_loss < t_valid_loss and valid_acc >= t_valid_acc:
-                target_model = model
-                t_valid_loss, t_valid_acc = valid_loss, valid_acc
-            elif t_valid_loss == 0:
-                t_valid_loss, t_valid_acc = valid_loss, valid_acc
-            p_train_loss, p_train_acc, p_valid_loss, p_valid_acc = train_loss, train_acc, valid_loss, valid_acc 
+        for epoch in seq(range(1, MAX_EPOCH + 1), "Training model"):
+            do_train(model, train, loss, optimizer)
+            valid_loss, valid_acc = evaluate(model, test, loss)
+
+            if (valid_loss < t_valid_loss and valid_acc >= t_valid_acc) or t_valid_loss == 0:
+                t_epoch, t_valid_loss, t_valid_acc = epoch, valid_loss, valid_acc
+                save_model(model)
+        
+        model = load_model()
+        pprint(f"Using model obtained at epoch={t_epoch} with VaLoss={t_valid_loss:.3f}, VaAcc={t_valid_acc:.3f}")
+
+    preprocessor(query, nlp)
+    query_loss, query_acc = evaluate(model, test, loss, doPrint=True)
+    pprint(f"Query Loss={query_loss:.3f}, Acc={query_acc:.3f}")
